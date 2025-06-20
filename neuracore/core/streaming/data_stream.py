793c2942902c9b4c5feaf6821da3504839568f54
Cougar Tasker <cougartasker@gmail.com>
2025-06-20 09:20:51 +0000

    wip: removed lossy encoder

--- neuracore/core/streaming/data_stream.py
+++ neuracore/core/streaming/data_stream.py
@@ -163,7 +163,6 @@ class VideoDataStream(DataStream):
         self.width = width
         self.height = height
         self._lossless_encoder: Optional[StreamingVideoUploader] = None
-        self._lossy_encoder: Optional[StreamingVideoUploader] = None
 
     def start_recording(self, recording_id: str) -> None:
         """Start video recording.
@@ -183,12 +182,8 @@ class VideoDataStream(DataStream):
         if self._lossless_encoder is None:
             raise TypeError("_lossless_encoder is None")
         lossless_upload_thread = self._lossless_encoder.finish()
-        if self._lossy_encoder is None:
-            raise TypeError("_lossy_encoder is None")
-        lossy_upload_thread = self._lossy_encoder.finish()
         self._lossless_encoder = None
-        self._lossy_encoder = None
-        return [lossless_upload_thread, lossy_upload_thread]
+        return [lossless_upload_thread]
 
     def log(self, data: np.ndarray, metadata: CameraData) -> None:
         """Log video frame data.
@@ -198,14 +193,9 @@ class VideoDataStream(DataStream):
             metadata: Camera metadata including timestamp and calibration
         """
         self._latest_data = data
-        if (
-            not self.is_recording()
-            or self._lossless_encoder is None
-            or self._lossy_encoder is None
-        ):
+        if not self.is_recording() or self._lossless_encoder is None:
             return
         self._lossless_encoder.add_frame(data, metadata)
-        self._lossy_encoder.add_frame(data, metadata)
 
 
 class DepthDataStream(VideoDataStream):
@@ -233,16 +223,6 @@ class DepthDataStream(VideoDataStream):
             depth_to_rgb,
             codec_context_options={"qp": "0", "preset": "ultrafast"},
         )
-        self._lossy_encoder = StreamingVideoUploader(
-            recording_id,
-            f"depths/{self.camera_id}/lossy",
-            self.width,
-            self.height,
-            depth_to_rgb,
-            video_name="lossy.mp4",
-            pixel_format="yuv420p",
-            codec="libx264",
-        )
 
 
 class RGBDataStream(VideoDataStream):
@@ -268,13 +248,4 @@ class RGBDataStream(VideoDataStream):
             width=self.width,
             height=self.height,
             codec_context_options={"qp": "0", "preset": "ultrafast"},
-        )
-        self._lossy_encoder = StreamingVideoUploader(
-            recording_id=recording_id,
-            path=f"rgbs/{self.camera_id}",
-            width=self.width,
-            height=self.height,
-            video_name="lossy.mp4",
-            pixel_format="yuv420p",
-            codec="libx264",
         )
